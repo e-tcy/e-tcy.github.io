d0aa41b92f3e0e9cfe42bba3a1369491ff946496
e-tcy <etcy-galex1t@outlook.com>
2024-09-29 23:25:26 +0700

    music player

--- music.js
+++ music.js
@@ -1,13 +1,43 @@
 const music = document.getElementById('music');
-const musicFiles = ["choppy beat.ogg", "trap.ogg", "hyperpop.ogg", ]
+const playButton = document.getElementById('playButton');
+let filename = ""
+const musicFiles = ["music\\choppy.ogg", "music\\trap.ogg", "music\\hyperpop.ogg"]
 
 function randPick() {
     const random = Math.floor(Math.random() * musicFiles.length)
     music.src = musicFiles[random]
     music.volume = 0.25
-    music.load()
-    music.play();
+    music.play()
+    music.pause()
+    filename = music.src.split('\\').pop();
+    document.getElementById('music-player').innerHTML = `now playing: ${filename}`;
 }
+
+playButton.addEventListener('click', function() {
+    if (music.muted) {
+        music.muted = false;
+        music.play()
+        document.getElementById('playButton').src = 'assets/pause.webp';
+        document.getElementById('music-player').innerHTML = `now playing: ${filename}`;
+        console.log('unmuted')
+    }
+    else if (music.paused) {
+        music.play()
+        document.getElementById('playButton').src = 'assets/pause.webp';
+        document.getElementById('music-player').innerHTML = `now playing: ${filename}`;
+        console.log('unpaused')
+    }
+    else if (!music.paused) {
+        music.pause()
+        document.getElementById('playButton').src = 'assets/play.webp';
+        document.getElementById('music-player').innerHTML = `paused: ${filename}`;
+        console.log('paused')
+    }
+    else {
+        console.log('how did we get here')
+    }
+
+  });
 
 music.addEventListener('ended', function() {
     randPick()
@@ -18,3 +48,4 @@ music.addEventListener('error', function() {
 })
 
 randPick()
+document.getElementById('music-player').innerHTML = `paused: ${filename}`;
